8c53bf30394cddb8d452f7a9b354212174c8c292
dxltcj <30287051+dxltcj@users.noreply.github.com>
2019-02-28 16:13:14 +0800

    Add files via upload

---
+++ clock.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>clock</title>
+</head>
+<body>
+	<div class="clock">
+		<div class="sid"></div>
+	</div>	
+</body>
+</html>
